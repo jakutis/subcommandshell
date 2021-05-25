d993bbf817e550640ca4e466176ac900211bedfc
Vytautas Jakutis <vytautas@jakut.is>
2021-05-25 16:45:21 +0300

    set default arguments, handle SIGINT and history

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 const readline = require('readline')
+const fs = require('fs')
 const child_process = require('child_process')
 
 const config = {
-  shell: process.argv[2],
-  prefix: process.argv[3],
-  command: process.argv[4],
-  default: process.argv[5]
+  command: process.argv[2],
+  shell: process.argv[3] || '/bin/sh',
+  prefix: process.argv[4] || (process.argv[2] + ' > '),
+  default: process.argv[5] || '',
+  sigintSuffix: process.argv[6] || '^C',
+  history: process.argv[7],
+}
+
+const getHistory = () => {
+  try {
+    return fs.readFileSync(config.history).toString().trim().split('\n')
+  } catch (err) {
+    return []
+  }
+}
+
+const setHistory = history => {
+  try {
+    fs.writeFileSync(config.history, history.join('\n') + '\n')
+  } catch (err) {
+  }
 }
 
 const interface = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  history: getHistory(config.history),
   removeHistoryDuplicates: true,
   historySize: 1000000
 })
 
+let ignoreInput = false
+
 const ask = () => {
   interface.question(config.prefix, input => {
+    if (ignoreInput) {
+      ignoreInput = false
+      return
+    }
     const subcommand = input.trim() || config.default
 
     try {
@@ -30,6 +55,15 @@ const ask = () => {
   })
 }
 
-interface.on('SIGINT', ask);
-interface.pause()
+interface.on('history', history => {
+  const acceptedHistory = history.filter(item => !item.endsWith(config.sigintSuffix))
+  history.splice.apply(history, [0, history.length].concat(acceptedHistory))
+  setHistory(history)
+})
+
+interface.on('SIGINT', () => {
+  ignoreInput = true
+  interface.write(config.sigintSuffix + '\n')
+  ask()
+})
 ask()
